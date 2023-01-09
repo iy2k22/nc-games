94fbb7322780b391cd692fdb173cb9b92ec1a2a2
Ibrahim Yusuf <iy830755@gmail.com>
2023-01-09 11:27:07 +0000

    enable cross origin resource sharing

--- controllers/index.js
+++ controllers/index.js
@@ -1,56 +1,70 @@
 const {
-    readCategories,
-    readReviews,
-    readReviewById,
-    readCommentsFromReview,
-    putComment
-} = require('../models');
+  readCategories,
+  readReviews,
+  readReviewById,
+  readCommentsFromReview,
+  putComment,
+} = require("../models");
 
 const getCategories = (req, res, next) => {
-    return readCategories().then((result) => {
-        res.status(200).send({ categories: result });
-    }).catch((err) => {
-        next(err);
+  return readCategories()
+    .then((result) => {
+      res.status(200).send({ categories: result });
+    })
+    .catch((err) => {
+      next(err);
     });
-}
+};
 
 const getReviews = (req, res, next) => {
-    readReviews().then((result) => {
-        const newResult = result.map((obj) => {
-            const newObj = {...obj};
-            newObj.comment_count = Number(obj.comment_count);
-            return newObj;
-        })
-        res.status(200).send({ reviews: newResult });
-    }).catch((err) => {
-        next(err);
+  readReviews()
+    .then((result) => {
+      const newResult = result.map((obj) => {
+        const newObj = { ...obj };
+        newObj.comment_count = Number(obj.comment_count);
+        return newObj;
+      });
+      res.status(200).send({ reviews: newResult });
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
 
 const getReviewById = (req, res, next) => {
-    readReviewById(req.params.review_id).then((review) => {
-        res.status(200).send({ review: review });
-    }).catch((err) => {
-        next(err);
+  readReviewById(req.params.review_id)
+    .then((review) => {
+      res.status(200).send({ review: review });
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
 
 const getCommentsFromReview = (req, res, next) => {
-    readCommentsFromReview(req.params.review_id).then((comments) => {
-        res.status(200).send({ comments: comments });
-    }).catch((err) => {next(err);})
-}
+  readCommentsFromReview(req.params.review_id)
+    .then((comments) => {
+      res.status(200).send({ comments: comments });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 const postComment = (req, res, next) => {
-    return putComment(req.params.review_id, req.body).then((comment) => {
-        res.status(201).send({ comment: comment });
-    }).catch((err) => {next(err);});
-}
+  return putComment(req.params.review_id, req.body)
+    .then((comment) => {
+      res.status(201).send({ comment: comment });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 module.exports = {
-    getCategories,
-    getReviews,
-    getReviewById,
-    getCommentsFromReview,
-    postComment
+  getCategories,
+  getReviews,
+  getReviewById,
+  getCommentsFromReview,
+  postComment,
 };
